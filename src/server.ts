37028f8f494dadbe75a1c67a205974e8f2e72dd0
Przemysław Kopeć <przemyslaw.kopec@contractors.roche.com>
2018-12-24 10:49:18 +0100

    TSLint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as WebSocket from 'ws';
-import { AddressInfo } from 'net';
+import * as express from "express";
+import * as http from "http";
+import { AddressInfo } from "net";
+import * as WebSocket from "ws";
 
 const app = express();
 
-//initialize a simple http server
+// initialize a simple http server
 const server = http.createServer(app);
 
-//initialize the WebSocket server instance
+// initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-interface ExtWebSocket extends WebSocket {
+interface IExtWebSocket extends WebSocket {
     isAlive: boolean;
 }
 
@@ -22,53 +22,55 @@ function createMessage(content: string, sender: string): string {
 export class Message {
     constructor(
         public content: string,
-        public sender: string
+        public sender: string,
     ) { }
 }
 
-wss.on('connection', (ws: WebSocket) => {
+wss.on("connection", (ws: WebSocket) => {
 
-    const extWs = ws as ExtWebSocket;
+    const extWs = ws as IExtWebSocket;
 
     extWs.isAlive = true;
 
-    ws.on('pong', () => {
+    ws.on("pong", () => {
         extWs.isAlive = true;
     });
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', (msg: string) => {
+    // connection is up, let"s add a simple simple event
+    ws.on("message", (msg: string) => {
         try {
-            const message = JSON.parse(msg) as Message;        
+            const message = JSON.parse(msg) as Message;
             wss.clients
-                .forEach(client => {                    
-                    client.send(createMessage(message.content, message.sender));                    
+                .forEach((client) => {
+                    client.send(createMessage(message.content, message.sender));
                 });
-        } catch(e) {
-            ws.send(createMessage('HELL no!', 'Server'));
+        } catch (e) {
+            ws.send(createMessage("HELL no!", "Server"));
             console.warn(e);
         }
     });
 
-    //send immediatly a feedback to the incoming connection
-    ws.send(createMessage('Welcome to HELL', 'Server'));
+    // send immediatly a feedback to the incoming connection
+    ws.send(createMessage("Welcome to HELL", "Server"));
 
-    ws.on('error', (err) => {
+    ws.on("error", (err) => {
         console.warn(`Client disconnected - reason: ${err}`);
-    })
+    });
 });
 
 setInterval(() => {
     wss.clients.forEach((ws: WebSocket) => {
-        const extWs = ws as ExtWebSocket;
-        if (!extWs.isAlive) return ws.terminate();
+        const extWs = ws as IExtWebSocket;
+        if (!extWs.isAlive) {
+            return ws.terminate();
+        }
         extWs.isAlive = false;
         ws.ping(null, undefined);
     });
 }, 30000);
 
-//start our server
-server.listen(process.env.PORT || 13666, () => {    
+// start our server
+server.listen(process.env.PORT || 13666, () => {
     const { port } = server.address() as AddressInfo;
     console.log(`Server started on port ${port} :)`);
 });
